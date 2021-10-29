92a44697e87ec40abcb12a57036ac560c27d0d1f
GimeVilches <86751159+GimeVilches@users.noreply.github.com>
2021-10-29 11:05:27 -0300

    NavBar

---
+++ src/Components/NavBar/navBar.js
@@ -0,0 +1,38 @@
+import "./NavBar.css";
+
+
+const NavBar= () =>{
+    return(
+        <nav className = "navBar">
+           
+            <div className = "itemNav">
+            
+                <ul>
+               
+                <h1>
+                Techno Stuff         
+                </h1>
+                
+                    <li>
+                        <a href= "">Productos</a>
+                    
+                    </li>
+					
+				    <li>
+                    <a href= "">Accesorios</a>
+                    </li>
+					<li>
+                    <a href= "">Contacto</a>
+                    </li>
+					
+                </ul>
+            
+            
+            </div>
+              
+            
+        </nav>
+    )
+
+}
+export default NavBar
